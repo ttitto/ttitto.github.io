cf8b2d1ae6e6ba31e32b75453ab071535503b311
ttitto <ttitto@users.noreply.github.com>
2014-08-12 02:20:17 +0300

    some responsive fixes, parallax slider finished final commit

--- my-mobile-hairdresser/scripts/parallaxCarousel.js
+++ my-mobile-hairdresser/scripts/parallaxCarousel.js
@@ -1,16 +1,26 @@
-/**
- * Created by ttitto on 14-8-11.
- */
-
-$('.carousel').on('slid.bs.carousel', function () {
-    animationRecall(".sliding");
+$('.carousel').on('slide.bs.carousel', function () {
+    animationRecall(".sliding-bigger");
+    animationRecall(".sliding-italic");
+    animationRecall(".sliding-bold");
 });
 
 function animationRecall(className) {
     var slidingEls = $(className);
-    for (var i=0;i<slidingEls.length; i++) {
+    for (var i = 0; i < slidingEls.length; i++) {
+
         var oldEl = slidingEls[i];
-        var newEl=oldEl.cloneNode(true);
+        var newEl = oldEl.cloneNode(true);
         oldEl.parentNode.replaceChild(newEl, oldEl);
     }
 }
+
+$(document).ready(function () {
+    $('#slide-menu-caller').sidr()
+});
+
+$(document).ready(function () {
+    $('.carousel').carousel({
+        interval: 6000, cycle: true
+    })
+});
+
